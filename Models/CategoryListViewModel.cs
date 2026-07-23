173fed4cccc8002bf8025f270c1876a109d08d8e
LeiYeeWynnThaung <20243039@aisstudent.ac.nz>
2026-07-23 20:22:43 +1200

    Initial RPMS frontend implementation

---
+++ Models/CategoryListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ResearchPublicationManagementSystem.Models
+{
+    public class CategoryListViewModel
+    {
+        public ListToolbarViewModel Toolbar { get; set; } = new();
+
+        public List<CategoryListItemViewModel> Categories { get; set; } = new();
+    }
+}
